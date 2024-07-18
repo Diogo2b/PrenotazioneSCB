99c3ac15e77a84a02b296b11eb007178846a2e08
Diogo2b <dioguinho2b@gmail.com>
2024-07-18 21:37:57 +0200

    Update RowControllerTest.php

--- tests/Controller/RowControllerTest.php
+++ tests/Controller/RowControllerTest.php
@@ -5,6 +5,7 @@ namespace App\Test\Controller;
 use App\Entity\Row;
 use App\Entity\Sector;
 use App\Entity\Tribune;
+use App\Entity\Seat; // Add this line
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
@@ -168,18 +169,29 @@ class RowControllerTest extends WebTestCase
         $this->manager->persist($tribune);
         $this->manager->persist($sector);
 
-        $fixture = new Row();
-        $fixture->setSigle('Value');
-        $fixture->setCapacity(30);
-        $fixture->setSector($sector);
+        $row = new Row();
+        $row->setSigle('Value');
+        $row->setCapacity(30);
+        $row->setSector($sector);
 
-        $this->manager->persist($fixture);
+        $seat1 = new Seat();
+        $seat1->setSeatNumber(1);
+        $seat1->setRow($row);
+
+        $seat2 = new Seat();
+        $seat2->setSeatNumber(2);
+        $seat2->setRow($row);
+
+        $this->manager->persist($row);
+        $this->manager->persist($seat1);
+        $this->manager->persist($seat2);
         $this->manager->flush();
 
-        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+        $this->client->request('GET', sprintf('%s%s', $this->path, $row->getId()));
         $this->client->submitForm('Delete');
 
         self::assertResponseRedirects('/row/');
         self::assertSame(0, $this->repository->count([]));
+        self::assertSame(0, $this->manager->getRepository(Seat::class)->count([]));
     }
 }
